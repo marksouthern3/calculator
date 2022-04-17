b70d350b548be84c31e476f99f1c1d18f128b950
Mark Southern <marksouthern3@gmail.com>
2022-04-17 18:37:38 +0100

    Start implementing button functionality
    The text buttons now print to the screen and we can use the DEL and
    AC buttons to remove text from the screen.

--- calculator.js
+++ calculator.js
@@ -26,3 +26,44 @@ function operate(a, b, operator) {
             return calculatorDivide(a, b);
     }
 }
+
+const screen = document.querySelector('#screen');
+const textButtons = document.querySelectorAll('.textButton');
+const funcButtons = document.querySelectorAll('.funcButton');
+
+let showingAnswer = true;
+
+textButtons.forEach(button => {
+    const key = button.getAttribute('data-key');
+    button.addEventListener('click', () => {
+        if (showingAnswer) {
+            screen.textContent = '';
+            showingAnswer = false;
+        }
+        screen.textContent += key;
+    });
+});
+
+funcButtons.forEach(button => {
+    const key = button.getAttribute('data-key');
+    switch (key) {
+        case 'DEL':
+            button.addEventListener('click', () => {
+                if (showingAnswer) {
+                    screen.textContent = '';
+                    showingAnswer = false;                    
+                } else { 
+                    screen.textContent = screen.textContent.slice(0, screen.textContent.length -1);
+                }
+            });
+            break;
+        case 'AC':
+            button.addEventListener('click', () => {
+                screen.textContent = '';
+                showingAnswer = false;
+
+            });
+            break;
+    }
+});
+
