0cda9061943cf13b4ab9c4071e677c34d52085e7
Mark Southern <marksouthern3@gmail.com>
2022-04-17 19:02:44 +0100

    Add operatorIndices array
    operatorIndices array keeps track of the indices of the operators in
    screen's text content. It is updated as operators are added or
    deleted.

--- calculator.js
+++ calculator.js
@@ -32,16 +32,34 @@ const textButtons = document.querySelectorAll('.textButton');
 const funcButtons = document.querySelectorAll('.funcButton');
 
 let showingAnswer = true;
+let answer = ''
+let operatorIndices = [];
+
+function evaluate(calcString) {
+
+}
 
 textButtons.forEach(button => {
     const key = button.getAttribute('data-key');
-    button.addEventListener('click', () => {
-        if (showingAnswer) {
-            screen.textContent = '';
-            showingAnswer = false;
-        }
-        screen.textContent += key;
-    });
+    if (key === '+' || key === '-' || key === '/' || key === '*') {
+        button.addEventListener('click', () => {
+            if (showingAnswer) {
+                screen.textContent = '';
+                showingAnswer = false;
+            }
+            screen.textContent += key;
+            operatorIndices.push(screen.textContent.length - 1);
+            console.log(operatorIndices);
+        });
+    } else {
+        button.addEventListener('click', () => {
+            if (showingAnswer) {
+                screen.textContent = '';
+                showingAnswer = false;
+            }
+            screen.textContent += key;
+        });
+    }
 });
 
 funcButtons.forEach(button => {
@@ -52,16 +70,28 @@ funcButtons.forEach(button => {
                 if (showingAnswer) {
                     screen.textContent = '';
                     showingAnswer = false;                    
-                } else { 
+                } else {
+                    if (operatorIndices.some(index => index === screen.textContent.length -1)) operatorIndices.pop();
                     screen.textContent = screen.textContent.slice(0, screen.textContent.length -1);
+                    console.log(operatorIndices);
                 }
             });
             break;
         case 'AC':
             button.addEventListener('click', () => {
                 screen.textContent = '';
+                answer = '';
+                operatorIndices = [];
                 showingAnswer = false;
-
+            });
+            break;
+        case 'ANS':
+            button.addEventListener('click', () => {
+                if (showingAnswer) {
+                    screen.textContent = '';
+                    showingAnswer = false;
+                }
+                screen.textContent += answer;
             });
             break;
     }
